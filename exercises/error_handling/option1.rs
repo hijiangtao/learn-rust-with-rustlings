988cada9cac7bb2b561bea98dd8a60b137473dfe
hijiangtao <hijiangtao@gmail.com>
2019-07-10 23:03:12 +0800

    update

--- exercises/error_handling/option1.rs
+++ exercises/error_handling/option1.rs
@@ -10,8 +10,10 @@ fn main() {
     let last = list.pop().unwrap();
     println!("The last item in the list is {:?}", last);
 
-    let second_to_last = list.pop().unwrap();
-    println!("The second-to-last item in the list is {:?}", second_to_last);
+    match list.pop() {
+        Some(second_to_last) => println!("The second-to-last item in the list is {:?}", second_to_last),
+        None => {}
+    }
 }
 
 
